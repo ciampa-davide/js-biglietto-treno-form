c83914767e46eacc5d9471de1d58206c1d0e59de
Davide <ciampad06@gmail.com>
2020-11-13 16:44:16 +0100

    parte finale js

--- js/script.js
+++ js/script.js
@@ -16,20 +16,33 @@ btnGenera.addEventListener("click",
     console.log(prezzo);
 
 
-    if (età == "minorenne" ) {
+    if (anni == "minorenne" ) {
       sconto= ((prezzo * 20) / 100);
       console.log(sconto);
       prezzoFinale= prezzo - sconto;
       console.log(prezzoFinale);
-    }else if (età > "over65") {
+      document.getElementById("offerta").innerHTML="sconto minorenni";
+    }else if (anni > "over65") {
       sconto= ((prezzo * 40) / 100);
       console.log(sconto);
       prezzoFinale= prezzo - sconto;
       console.log(prezzoFinale);
+      document.getElementById("offerta").innerHTML="sconto silver";
     }else{
       prezzoFinale = prezzo;
       console.log(prezzoFinale);
+      document.getElementById("offerta").innerHTML="Offerta standard";
     }
-    document.getElementById("treno").innerHTML= prezzoFinale.toPrecision (3);
+    document.getElementById("prezzofinale").innerHTML= prezzoFinale.toPrecision (3);
+
+    var nomeUtente= document.getElementById("nome").value;
+    document.getElementById("nomeBiglietto").innerHTML=nomeUtente;
+
+    var cp=Math.floor(Math.random()*99999) +90000;
+    document.getElementById("codice").innerHTML=cp;
+
+    var vagone=Math.floor(Math.random()*10) + 1;
+    document.getElementById("carrozza").innerHTML=vagone;
+
   }
 )
